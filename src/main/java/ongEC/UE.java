1b94613e69a53238e3f1f4231ddbbe7cb82f9896
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-03-14 15:29:47 +0100

    IngresosPublicos
    IngresosPublicos

--- src/main/java/ongEC/UE.java
+++ src/main/java/ongEC/UE.java
@@ -1,5 +1,54 @@
 package ongEC;
 
 public class UE {
+	
+	String nombreOrganizacion;
+	//Direccion direccion;
+	float importe;
+	String Descripcion;
+	
+	public String getNombreOrganizacion() {
+		return nombreOrganizacion;
+	}
+	
+	public void setNombreOrganizacion(String nombreOrganizacion) {
+		this.nombreOrganizacion = nombreOrganizacion;
+	}
+	
+//	public Direccion getDireccion() {
+//		return direccion;
+//	}
+	
+//	public void setDireccion(Direccion direccion) {
+//		this.direccion = direccion;
+//	}
+	
+	public float getImporte() {
+		return importe;
+	}
+	
+	public void setImporte(float importe) {
+		this.importe = importe;
+	}
+	
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	
+	public UE(String nombreOrganizacion, float importe, String descripcion) { //Falta el campo direccion
+		super();
+		this.nombreOrganizacion = nombreOrganizacion;
+//		this.direccion = direccion;
+		this.importe = importe;
+		Descripcion = descripcion;
+	}
+	
+	public UE() {
+		super();
+	}		
 
 }
