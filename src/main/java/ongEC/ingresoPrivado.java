7ece5b7dc3c1f8cf65931d5cfe7441ed341a78e6
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-03-14 15:20:13 +0100

    IngresoPrivado
    IngresoPrivado

--- src/main/java/ongEC/ingresoPrivado.java
+++ src/main/java/ongEC/ingresoPrivado.java
@@ -1,5 +1,35 @@
 package ongEC;
 
-public class ingresoPrivado {
+import java.sql.Date;
 
+public class ingresoPrivado {
+	
+	int idIngreso;
+	Date fechaIngreso;
+	
+	public int getIdIngreso() {
+		return idIngreso;
+	}
+
+	public void setIdIngreso(int idIngreso) {
+		this.idIngreso = idIngreso;
+	}
+
+	public Date getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(Date fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public ingresoPrivado(int idIngreso, Date fechaIngreso) {
+		super();
+		this.idIngreso = idIngreso;
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public ingresoPrivado() {
+		super();
+	}		
 }
