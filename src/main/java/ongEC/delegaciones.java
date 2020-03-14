c5952c71d44b54021df13b27d922c1af6e44248e
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-03-14 16:31:26 +0100

    DefinicionClases
    DefinicionClases

--- src/main/java/ongEC/delegaciones.java
+++ src/main/java/ongEC/delegaciones.java
@@ -1,5 +1,43 @@
 package ongEC;
 
 public class delegaciones {
+	
+	String nombre;
+	String cip;
+	Direccion direccion;
 
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getCip() {
+		return cip;
+	}
+	
+	public void setCip(String cip) {
+		this.cip = cip;
+	}
+	
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+
+	public delegaciones(String nombre, String cip, Direccion direccion) {
+		super();
+		this.nombre = nombre;
+		this.cip = cip;
+		this.direccion = direccion;
+	}
+
+	public delegaciones() {
+		super();
+	}	
 }
