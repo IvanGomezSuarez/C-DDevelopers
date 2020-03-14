b3caec04f3809b9b9b3d2b066a36a6e98f3d5594
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-03-14 15:20:39 +0100

    IngresosPrivados2
    IngresosPrivados2

--- src/main/java/ongEC/aportPuntualPartic.java
+++ src/main/java/ongEC/aportPuntualPartic.java
@@ -1,5 +1,44 @@
 package ongEC;
 
 public class aportPuntualPartic {
-
+	
+	String nombreOrganizacion;
+	//Direccion direccion;
+	float importe;
+	String Descripcion;
+	
+	public String getNombreOrganizacion() {
+		return nombreOrganizacion;
+	}
+	public void setNombreOrganizacion(String nombreOrganizacion) {
+		this.nombreOrganizacion = nombreOrganizacion;
+	}
+//	public Direccion getDireccion() {
+//		return direccion;
+//	}
+//	public void setDireccion(Direccion direccion) {
+//		this.direccion = direccion;
+//	}
+	public float getImporte() {
+		return importe;
+	}
+	public void setImporte(float importe) {
+		this.importe = importe;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public aportPuntualPartic(String nombreOrganizacion, float importe, String descripcion) { //Falta el campo direccion
+		super();
+		this.nombreOrganizacion = nombreOrganizacion;
+//		this.direccion = direccion;
+		this.importe = importe;
+		Descripcion = descripcion;
+	}
+	public aportPuntualPartic() {
+		super();
+	}		
 }
