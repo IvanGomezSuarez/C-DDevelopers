aa5faadbbca23ac8bd82cc9f24402b3dd98dc7a9
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-03-14 19:58:54 +0100

    implementacion clase ingresos y las enumeradas

--- src/main/java/ongEC/cuotaSocio.java
+++ src/main/java/ongEC/cuotaSocio.java
@@ -1,5 +1,13 @@
 package ongEC;
 
+
+/**
+ * esta clase enumera los tipos de cuota de socio existentes.
+ * 
+ * IVAN 
+ */
+
 public enum CuotaSocio {
 
+	MENSUAL, TRIMESTRAL, ANUAL
 }
--- src/main/java/ongEC/ingreso.java
+++ src/main/java/ongEC/ingreso.java
@@ -1,5 +1,72 @@
 package ongEC;
 
-public interface ingreso {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.GregorianCalendar;
 
+public class Ingreso {
+	
+	//ATRIBUTOS
+	private int idIngreso;
+	private java.util.GregorianCalendar fechaIngreso;
+	
+	
+	//CONSTRUCTORES
+	
+	/**
+	 * este constructor crea objeto e inicialia los atributos.
+	 * @param cifOng 
+	 */
+	public Ingreso(int idIngreso, java.util.GregorianCalendar fechaIngreso) {
+		
+		this.setIdIngreso(idIngreso);
+		this.setFechaIngreso(fechaIngreso);
+	}
+	
+	
+	
+	//METODOS
+
+	private void addIngresoPublico() {
+}
+	
+	private void addIngresoPrivado() {
+}
+	
+	
+	private void delIngresoPublico() {
+}
+	
+	
+	private void delIngresoPrivado() {
+}
+
+
+
+	public java.util.GregorianCalendar getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+
+
+	public void setFechaIngreso(java.util.GregorianCalendar fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+
+
+	public int getIdIngreso() {
+		return idIngreso;
+	}
+
+
+
+	public void setIdIngreso(int idIngreso) {
+		this.idIngreso = idIngreso;
+	}
+	
+	
 }
