1ef9be3eef340ee01bac40afd7c9c7704c71571d
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-03-14 16:40:35 +0100

    CambiosFinalesv2
    CambiosFinalesv2

--- src/main/java/ongEC/lineaAcccion.java
+++ src/main/java/ongEC/lineaAcccion.java
@@ -1,5 +1,46 @@
 package ongEC;
 
+import java.util.List;
+
 public class lineaAcccion {
 
+	actualLineasAccion lineaAccion;
+	List<subLineaAccion>subLineaAccion;
+	String descripcionAccion;
+	
+	public actualLineasAccion getLineaAccion() {
+		return lineaAccion;
+	}
+	
+	public void setLineaAccion(actualLineasAccion lineaAccion) {
+		this.lineaAccion = lineaAccion;
+	}
+	
+	public List<subLineaAccion> getSubLineaAccion() {
+		return subLineaAccion;
+	}
+	
+	public void setSubLineaAccion(List<subLineaAccion> subLineaAccion) {
+		this.subLineaAccion = subLineaAccion;
+	}
+	
+	public String getDescripcionAccion() {
+		return descripcionAccion;
+	}
+	
+	public void setDescripcionAccion(String descripcionAccion) {
+		this.descripcionAccion = descripcionAccion;
+	}
+
+	public lineaAcccion(actualLineasAccion lineaAccion, List<ongEC.subLineaAccion> subLineaAccion,
+			String descripcionAccion) {
+		super();
+		this.lineaAccion = lineaAccion;
+		this.subLineaAccion = subLineaAccion;
+		this.descripcionAccion = descripcionAccion;
+	}
+
+	public lineaAcccion() {
+		super();
+	}	
 }
