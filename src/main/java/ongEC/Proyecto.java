e49567590a2e8cd96cc1f8c797b3c30abb151283
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-03-18 12:11:59 +0100

    Clase Proyecto
    Clase Proyecto

--- src/main/java/ongEC/Proyecto.java
+++ src/main/java/ongEC/Proyecto.java
@@ -1,5 +1,159 @@
 package ongEC;
 
-public class Proyecto {
+import java.sql.Date;
+import java.util.List;
 
+public class Proyecto {
+	
+	int idProyecto;
+	String nombreProyecto;
+	String pais;
+	String localizacion;
+	LineaAccion lineaAccion;
+	List<SubLineaAccion> subLineaAccion;
+	Date fechaInicio;
+	Date fechaFin;
+	List<Socio> socioLocal;
+	String financiador;
+	Double financiacionAportada;
+	String codigoProyecto;
+	String AccionesRealizar;
+	List<Miembros> miembrosAsignados;
+	
+	public int getIdProyecto() {
+		return idProyecto;
+	}
+	
+	public void setIdProyecto(int idProyecto) {
+		this.idProyecto = idProyecto;
+	}
+	
+	public String getNombreProyecto() {
+		return nombreProyecto;
+	}
+	
+	public void setNombreProyecto(String nombreProyecto) {
+		this.nombreProyecto = nombreProyecto;
+	}
+	
+	public String getPais() {
+		return pais;
+	}
+	
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	public String getLocalizacion() {
+		return localizacion;
+	}
+	
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
+	
+	public LineaAccion getLineaAccion() {
+		return lineaAccion;
+	}
+	
+	public void setLineaAccion(LineaAccion lineaAccion) {
+		this.lineaAccion = lineaAccion;
+	}
+	
+	public List<SubLineaAccion> getSubLineaAccion() {
+		return subLineaAccion;
+	}
+	
+	public void setSubLineaAccion(List<SubLineaAccion> subLineaAccion) {
+		this.subLineaAccion = subLineaAccion;
+	}
+	
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	
+	public List<Socio> getSocioLocal() {
+		return socioLocal;
+	}
+	
+	public void setSocioLocal(List<Socio> socioLocal) {
+		this.socioLocal = socioLocal;
+	}
+	
+	public String getFinanciador() {
+		return financiador;
+	}
+	
+	public void setFinanciador(String financiador) {
+		this.financiador = financiador;
+	}
+	
+	public Double getFinanciacionAportada() {
+		return financiacionAportada;
+	}
+	
+	public void setFinanciacionAportada(Double financiacionAportada) {
+		this.financiacionAportada = financiacionAportada;
+	}
+	
+	public String getCodigoProyecto() {
+		return codigoProyecto;
+	}
+	
+	public void setCodigoProyecto(String codigoProyecto) {
+		this.codigoProyecto = codigoProyecto;
+	}
+	
+	public String getAccionesRealizar() {
+		return AccionesRealizar;
+	}
+	
+	public void setAccionesRealizar(String accionesRealizar) {
+		AccionesRealizar = accionesRealizar;
+	}
+	
+	public List<Miembros> getMiembrosAsignados() {
+		return miembrosAsignados;
+	}
+	
+	public void setMiembrosAsignados(List<Miembros> miembrosAsignados) {
+		this.miembrosAsignados = miembrosAsignados;
+	}
+
+	public Proyecto(int idProyecto, String nombreProyecto, String pais, String localizacion, LineaAccion lineaAccion,
+			List<SubLineaAccion> subLineaAccion, Date fechaInicio, Date fechaFin, List<Socio> socioLocal,
+			String financiador, Double financiacionAportada, String codigoProyecto, String accionesRealizar,
+			List<Miembros> miembrosAsignados) {
+		super();
+		this.idProyecto = idProyecto;
+		this.nombreProyecto = nombreProyecto;
+		this.pais = pais;
+		this.localizacion = localizacion;
+		this.lineaAccion = lineaAccion;
+		this.subLineaAccion = subLineaAccion;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.socioLocal = socioLocal;
+		this.financiador = financiador;
+		this.financiacionAportada = financiacionAportada;
+		this.codigoProyecto = codigoProyecto;
+		AccionesRealizar = accionesRealizar;
+		this.miembrosAsignados = miembrosAsignados;
+	}
+
+	public Proyecto() {
+		super();
+	}
 }
