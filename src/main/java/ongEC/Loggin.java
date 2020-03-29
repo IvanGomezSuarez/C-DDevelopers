369fe552fd1f2dca1ac2f5aa33b499f34b69f5ec
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-03-29 18:23:51 +0200

    1
    1

--- src/main/java/ongEC/Loggin.java
+++ src/main/java/ongEC/Loggin.java
@@ -111,14 +111,18 @@ public static void loggin() throws IOException, JAXBException {
               System.out.println("Introduce tu nombre contraseña");
               respuestaPass = br.readLine();
               		    
-              if (respuestaPass == ivan) {
-              	Main.opcionesMenuPrincipal();
+              if (respuestaPass.equals("ivan")) {
+            	  System.out.println("Buscando miembro...");
+            	  System.out.println("Miembro encontrado Buenas" + " " + respuestaNombre);
+              	return;
+            	}else {
+            		 System.out.println("Miembro no encontrado");
+            		 Loggin.loggin();
             	}
               break;
            
          case 2:
-        	  Miembros miembros = new Miembros();
-        	  Miembros.abrirSesion();
+        	  abrirSesion();
               break;
             
            case 3:
@@ -127,10 +131,75 @@ public static void loggin() throws IOException, JAXBException {
               break;
         }
     }
+
+public static void abrirSesion() throws IOException, JAXBException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	int respuestaOpcion = 0;
+	Integer[] opcionesValidas = {1, 2, 3};
+	String respuestaNuevaAccion;
+
+	System.out.println("\n************************");
+	System.out.println(" Opciones de Administrador");
+	System.out.println("**************************");
+
+	do {
+
+		System.out.println("\nPor favor, introduce el número de la acción que deseas realizar: ");
+		System.out.println("1 - Dar de alta una delegacion");
+		System.out.println("2 - Imprimir listado de delegaciones");
+		System.out.println("3 - Dar de baja una delegacion");
+		System.out.println("4 - Salir");
+
+		try {
+			respuestaOpcion = Integer.parseInt(br.readLine());
+		} catch(NumberFormatException nfe) {
+			System.out.println("Los caracteres introducidos no son válidos.");
+		}
+
+	} while (!Arrays.asList(opcionesValidas).contains(respuestaOpcion));
+
+	switch(respuestaOpcion) {
+
+	case 1:
+		Ong.addDelegacion();
+
+		do {
+
+			do {
+				System.out.println("¿Deseas dar de alta otra delegacion? (S/N)");
+				respuestaNuevaAccion = br.readLine();
+			} while (!respuestaNuevaAccion.equalsIgnoreCase("s") && !respuestaNuevaAccion.equalsIgnoreCase("n"));
+
+			if (respuestaNuevaAccion.equalsIgnoreCase("s")) {
+				Ong.addDelegacion();
+			}
+
+		} while (!respuestaNuevaAccion.equalsIgnoreCase("n"));
+
+		abrirSesion();
+
+		break;
+
+	case 2:
+		Ong.printDelegaciones();
+		abrirSesion();
+		break;
+		
+		
+	case 3:
+		Ong.delDelegacion();
+		abrirSesion();
+
+		break;	
+		
+
+	case 4:
+		System.out.println("La sesión se ha cerrado con éxito.");
+		System.exit(0);
+		break;
+	}
 }
-
-
-
+}
 
 
 
