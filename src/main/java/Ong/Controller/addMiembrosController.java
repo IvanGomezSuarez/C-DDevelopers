5400f957676d4cd60227cd3932afe0bd9bd69796
ygarciai <62152443+ygarciai@users.noreply.github.com>
2020-05-15 15:34:34 +0200

    Update addMiembrosController.java

--- src/main/java/Ong/Controller/addMiembrosController.java
+++ src/main/java/Ong/Controller/addMiembrosController.java
@@ -121,23 +121,44 @@ public class addMiembrosController {
     @FXML
     void guardarCambios(ActionEvent event) {
     	emf = Persistence.createEntityManagerFactory("persistencia2");
+    	miembro=new MiembroJpaController(emf);
+    	voluntario=new VoluntarioJpaController(emf);
+    	colaborador=new ColaboradorJpaController(emf);
+    	personal=new PersonalJpaController(emf);
     	direccion=new DireccionesUsuarioJpaController(emf);
+    	
+    	Ong.Models.DireccionesUsuario direccionNueva = new Ong.Models.DireccionesUsuario();
+		Ong.Models.Miembro miembroNuevo=new Ong.Models.Miembro();
+		Ong.Models.Personal personalNuevo=new Ong.Models.Personal();
+		
+		miembroNuevo.setNombreMiembro(text_Nombre.getText());
+		miembroNuevo.setApellido1(text_Apellido1.getText());
+		miembroNuevo.setApellido2(text_Apellido2.getText());
+		miembroNuevo.setDni(text_Dni.getText());
+		miembroNuevo.setNombreUsuario(text_Usuario.getText());
+		miembroNuevo.setTelefono(text_Telefono.getText());
+		miembroNuevo.setPass(text_Password.getText());
+		miembroNuevo.setRol(choice_Rol.getTypeSelector());
+
+		direccionNueva.setIdDireccion(5);
+		direccionNueva.setCp(text_CP.getText());
+		direccionNueva.setEscalera(text_Escalera.getText());
+		direccionNueva.setLocalidad(text_Localidad.getText());
+		direccionNueva.setNumero(Integer.parseInt(text_Numero.getText()));
+		direccionNueva.setPais(text_Pais.getText());
+		direccionNueva.setProvincia(text_Provincia.getText());
+		direccionNueva.setPuerta(text_Puerta.getText());
+		direccionNueva.setTipoVia(text_Via.getText());
     	
     	if(choice_comprobacion_Miembro.getValue().equals("VOLUNTARIO")){
     		
-    	}else{
-    		Ong.Models.DireccionesUsuario direccionNueva = new Ong.Models.DireccionesUsuario();
-    		direccionNueva.setIdDireccion(5);
-    		direccionNueva.setCp(text_CP.getText());
-    		direccionNueva.setEscalera(text_Escalera.getText());
-    		//direccionNueva.setIdDireccion(text_I);
-    		direccionNueva.setLocalidad(text_Localidad.getText());
-    		direccionNueva.setNumero(Integer.parseInt(text_Numero.getText()));
-    		direccionNueva.setPais(text_Pais.getText());
-    		direccionNueva.setProvincia(text_Provincia.getText());
-    		direccionNueva.setPuerta(text_Puerta.getText());
-    		direccionNueva.setTipoVia(text_Via.getText());
+    	}else if (choice_comprobacion_Miembro.getValue().equals("TRABAJADOR")){
+    		personalNuevo.setFechaAlta(text_FechaAlta.getText());
+    		personalNuevo.setFechaBaja(text_FechaBaja.getText());
+    		
     		direccion.create(direccionNueva);
+    	}else {
+    		
     	}
 
     }
